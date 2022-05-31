adb0b150af8e854d0c67e628bac300a78958adc0
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-05-31 13:39:14 -0400

    Added Routing For Components

--- banking-application/src/app/app.module.ts
+++ banking-application/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { DepositComponent } from './components/deposit/deposit.component';
@@ -12,24 +11,44 @@ import { WithdrawalComponent } from './components/withdrawal/withdrawal.componen
 import { CheckingsComponent } from './components/checkings/checkings.component';
 import { SavingsComponent } from './components/savings/savings.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+
+const routes: Routes = [
+
+  { path: '', component: LandingPageComponent },
+  { path: 'savings', component: SavingsComponent },
+  { path: 'checkings', component: CheckingsComponent },
+  { path: 'deposit', component: DepositComponent },
+  { path: 'withdraw', component: WithdrawalComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+
+]
+
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MainContentComponent,
     LandingPageComponent,
     LoginComponent,
     DepositComponent,
     WithdrawalComponent,
     CheckingsComponent,
     SavingsComponent,
-    ProfileComponent
+    ProfileComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
