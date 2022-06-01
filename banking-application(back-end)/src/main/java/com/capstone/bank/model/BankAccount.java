54be7263220eaaaa07c1ca185a66f9e1ab15c78b
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-06-01 12:52:45 -0400

    Back End
    Uploaded initial Spring Boot Back End application with basic CRUD methods

---
+++ banking-application(back-end)/src/main/java/com/capstone/bank/model/BankAccount.java
@@ -0,0 +1,67 @@
+package com.capstone.bank.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "accounts")
+public class BankAccount {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "account_number")
+	private Long accountNumber;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "balance")
+	private Long balance;
+	
+
+	public Long getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(Long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Long balance) {
+		this.balance = balance;
+	}
+
+	
+
+	
+
+
+	
+}
