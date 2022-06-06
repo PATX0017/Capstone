70835724f292b75a34ecc7faafe9ca885cb81743
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-06-06 11:03:44 -0400

    UI Edits
    Removed Side bar. Edited page responsiveness to accommodate smaller screens.

--- banking-application/src/app/components/landing-page/landing-page.component.ts
+++ banking-application/src/app/components/landing-page/landing-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BankService } from 'src/app/services/bank.service';
 import { Account } from 'src/app/common/account';
 import { ActivatedRoute } from '@angular/router';
-
+import { Chart, ChartItem, ChartConfiguration, registerables } from 'node_modules/chart.js';
 
 
 @Component({
@@ -13,17 +13,84 @@ import { ActivatedRoute } from '@angular/router';
 export class LandingPageComponent implements OnInit {
 
   account = new Account();
-
+  balance!: number;
   constructor(private bankService: BankService,
     private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     //accounts/:id = account/2
     const number: number = +this.route.snapshot.paramMap.get('id')!;
-    this.bankService.getSpecificAccount(number).subscribe(res => { this.account = res });
+    this.bankService.getSpecificAccount(number).subscribe(res => {
+      this.account = res;
+
+    });
+    this.createChart1(1, 2, 3);
+    this.createChart2();
 
   }
 
 
 
+
+  createChart1(num1: number, num2: number, num3: number): void {
+    Chart.register(...registerables);
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', ''],
+      datasets: [{
+        label: 'Savings Account Growth',
+        backgroundColor: 'rgb(28, 200, 137)',
+        borderColor: 'rgb(28, 200, 137)',
+        data: [num1, num2, num3, 20, 30, 45],
+      }]
+
+    };
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          display: false
+        }
+      }
+    }
+    const config: ChartConfiguration = {
+      type: 'line',
+      data: data,
+      options: options
+    }
+    const chartItem: ChartItem = document.getElementById('my-chart2') as ChartItem
+    new Chart(chartItem, config)
+  }
+  createChart2(): void {
+    Chart.register(...registerables);
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May'],
+      datasets: [{
+        label: 'Checkings Account Growth',
+        backgroundColor: 'rgb(78, 114, 223)',
+        borderColor: 'rgb(78, 114, 223)',
+        data: [10, 5, 2, 20, 30, 45],
+      }]
+
+    };
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          display: false
+        }
+      }
+    }
+    const config: ChartConfiguration = {
+      type: 'line',
+      data: data,
+      options: options
+    }
+    const chartItem: ChartItem = document.getElementById('my-chart1') as ChartItem
+    new Chart(chartItem, config)
+  }
+
+
+  depositModal() {
+
+  }
 }
--- banking-application/src/app/components/nav-bar/nav-bar.component.ts
+++ banking-application/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BankService } from 'src/app/services/bank.service';
+import { Account } from 'src/app/common/account';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +13,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+
+  account = new Account();
+
+
+
+  constructor(public route: ActivatedRoute,
+    private bankService: BankService,
+    public router: Router,
+    public location: Location) {
+
+    router.events.subscribe(val => {
+      if (router.url == "/account/1") {
+        this.getAccountInfo();
+      }
+    });
+  }
+
 
   ngOnInit(): void {
+    //accounts/:id = account/2
+
+  }
+
+  getAccountInfo() {
+    const numbers: number = +this.route.snapshot.paramMap.get('id')!;
+    console.log(numbers);
+    this.bankService.getSpecificAccount(numbers).subscribe(res => { this.account = res });
+
   }
 
 }
+
